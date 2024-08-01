b207f8254e96ad2eded01e878412ea78cd31b5f0
MukhammadumarDeveloper <Muhammadumar_Mahmudov@student.itpu.uz>
2024-08-01 11:21:25 +0500

    last changes

--- src/main/java/com/epam/learning/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.epam.learning.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-
-    UserDetailsService userDetailService();
-}
